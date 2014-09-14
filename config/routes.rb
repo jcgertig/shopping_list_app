797d1641255de6e815dde8210b0f9d8b286b7ff0
Jonathan Gertig <jcgertig@gmail.com>
2014-09-14 15:22:04 -0400

    basic angular routing and layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  
+  root 'application#index'
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+
   devise_for :users
+
+  get '*path', to: 'application#index'
 end
