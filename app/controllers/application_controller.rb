393497acec245c29fab860780686078a3b078d78
Jonathan Gertig <jcgertig@gmail.com>
2014-09-17 23:37:09 -0400

    custom sign in check

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  before_action :authenticate_user!, only: [:index]
-
   def index
+    unless user_signed_in?
+      redirect_to new_user_session_path, notice: "You have to be siged in to use this app."
+    end
   end
 end
